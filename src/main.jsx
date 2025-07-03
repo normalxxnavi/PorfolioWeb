e1fe3ad044b53034a351151182285b789a25a42b
Javier Lopez <javierlopezvelasco51@gmail.com>
2025-07-03 20:43:28 +0200

    Instalacion y Configuracion de fluent ui v9

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,13 @@
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.jsx";
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { FluentProvider, webLightTheme } from '@fluentui/react-components';
 
-createRoot(document.getElementById("root")).render(<App />);
+import App from './App';
+
+const root = createRoot(document.getElementById('root'));
+
+root.render(
+  <FluentProvider theme={webLightTheme}>
+    <App />
+  </FluentProvider>,
+);
